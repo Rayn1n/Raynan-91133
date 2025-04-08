46798f8541695e44e81db74cb1ee36f80ff63767
Rayn1n <raynancarvalho.silva@gmail.com>
2025-04-08 11:08:18 -0300

    Atividade Cadastro - 08/04/25

---
+++ 91133/Sistema/script.js
@@ -0,0 +1,24 @@
+document.getElementById("loginFrom").addEventListener("submit", function(event){
+    event.preventDefault(); //! Impede o envio do formulário
+    
+    const username = document.getElementById("username").value; //! Obter o valor do elemento
+    const password = document.getElementById("password").value;
+    
+    //! Validação dos campos
+        if(!username || !password){
+            alert("Por favor, preencha todos os campos.");
+            return;
+        }
+        else if(password.length < 8){
+            alert("A senha deve ter pelo menos 8 cracateres.");
+            return;
+        }
+    
+        //! Salva o nome de usuário no localStorage
+        localStorage.setItem("username", username)
+    
+        window.location.href ="cadastro.html";
+        alert("Login bem sucedido!");
+    
+    
+    });
---
+++ 91133/Sistema/script2.js
@@ -0,0 +1,21 @@
+document.getElementById("cadastro").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const produto = document.getElementById("produto").value;
+    const preco = parseFloat(document.getElementById("preco").value);
+    const quantidade = parseInt(document.getElementById("quantidade").value);
+    const totalInput = document.getElementById("total");
+
+    if (!produto || !(preco) || !(quantidade)) {
+        alert("Por favor, preencha todos os campos corretamente.");
+        return;
+    }
+
+    if (quantidade < 1) {
+        alert("Deve haver ao menos um produto para ser cadastrado.");
+        return;
+    }
+
+    const total = preco * quantidade;
+    totalInput.value = total.toFixed(2).replace('.', ','); 
+});
