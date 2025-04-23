c985f0354bd8e7c37715c382bd5c61003bb8ae7a
Rayn1n <raynancarvalho.silva@gmail.com>
2025-04-23 11:02:02 -0300

    aula 23/04/25

---
+++ 91133/Segurança/api/api.php
@@ -0,0 +1,21 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST'){
+    header("Location: http://localhost:8081/app/index.html");
+    exit;
+}
+
+include 'autenticar.php';
+
+include 'conexao.php';
+
+include 'uinsere.php';
+
+include '/view/conecta.php';
+
+echo "<script>
+        alert(\"Cadastro realizado com sucesso!\");
+        window.location.href = 'http://localhost:8081/app/index.html';
+    </script>";
+
+?>
